0376d7d82ba5cffcc4bc454490d0020d67c5bf47
maoyuan.li@zhenai.com <maoyuan.li@zhenai.com>
2023-03-28 00:41:29 +0800

    bug fixed

--- chatgpt-adapter/src/main/java/com/szmengran/chatgpt/wap/CompletionController.java
+++ chatgpt-adapter/src/main/java/com/szmengran/chatgpt/wap/CompletionController.java
@@ -1,8 +1,11 @@
 package com.szmengran.chatgpt.wap;
 
 import com.szmengran.chatgpt.api.ChatCompletionFacade;
+import com.szmengran.chatgpt.api.CompletionFacade;
 import com.szmengran.chatgpt.dto.chat.ChatCompletionCO;
 import com.szmengran.chatgpt.dto.chat.ChatCompletionCreateCmd;
+import com.szmengran.chatgpt.dto.completion.CompletionCO;
+import com.szmengran.chatgpt.dto.completion.CompletionCreateCmd;
 import com.szmengran.cola.dto.SingleResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,11 +25,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CompletionController {
     
     @Resource
-    private ChatCompletionFacade chatCompletionFacade;
+    private CompletionFacade completionFacade;
     
     @Operation(summary = "Creates a completion for the chat message")
     @PostMapping("/v1/completions")
-    public SingleResponse<ChatCompletionCO> chatCompletions(@RequestBody ChatCompletionCreateCmd chatCompletionCreateCmd) {
-        return chatCompletionFacade.chatCompletion(chatCompletionCreateCmd);
+    public SingleResponse<CompletionCO> completions(@RequestBody CompletionCreateCmd completionCreateCmd) {
+        return completionFacade.completions(completionCreateCmd);
     }
 }
