27e5acf26abb2cb7de7080e9c1b9d2d040a5a75d
maoyuan.li@zhenai.com <maoyuan.li@zhenai.com>
2023-06-17 23:33:23 +0800

    添加用户注册与登录

--- chatgpt-adapter/src/main/java/com/szmengran/chatgpt/wap/UserController.java
+++ chatgpt-adapter/src/main/java/com/szmengran/chatgpt/wap/UserController.java
@@ -1,9 +1,11 @@
 package com.szmengran.chatgpt.wap;
 
-import com.szmengran.chatgpt.api.UserFacade;
+import com.szmengran.authorization.dto.cqe.UserRegisterCmd;
+import com.szmengran.chatgpt.app.user.UserServiceImpl;
 import com.szmengran.chatgpt.dto.user.TokenCO;
 import com.szmengran.chatgpt.dto.user.TokenQueryCmd;
 import com.szmengran.chatgpt.infrastructure.oauth2.config.ClientPrincipalProperties;
+import com.szmengran.cola.dto.Response;
 import com.szmengran.cola.dto.SingleResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -18,12 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author <a href="mailto:android_li@sina.cn">MaoYuan.Li</a>
  * @Date 2023/6/8 23:01
  */
-@Tag(name = "ChatGPT登录服务API")
+@Tag(name = "ChatGPT用户服务API")
 @RestController
-public class LoginController {
+public class UserController {
 
 	@Resource
-	private UserFacade userFacade;
+	private UserServiceImpl userService;
 
 	@Resource
 	private ClientPrincipalProperties clientPrincipalProperties;
@@ -32,7 +34,13 @@ public class LoginController {
 	@PostMapping("/login")
 	public SingleResponse<TokenCO> login(@RequestBody TokenQueryCmd tokenQueryCmd) {
 		tokenQueryCmd.setScope(clientPrincipalProperties.getScope());
-		TokenCO tokenDTO = userFacade.login(tokenQueryCmd);
-		return SingleResponse.of(tokenDTO);
+		TokenCO tokenCO = userService.login(tokenQueryCmd);
+		return SingleResponse.of(tokenCO);
+	}
+
+	@Operation(summary = "用户注册")
+	@PostMapping("/register")
+	public Response register(@RequestBody UserRegisterCmd userRegisterCmd) {
+		return userService.register(userRegisterCmd);
 	}
 }
--- chatgpt-app/src/main/java/com/szmengran/chatgpt/app/user/UserServiceImpl.java
+++ chatgpt-app/src/main/java/com/szmengran/chatgpt/app/user/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.szmengran.chatgpt.app.user;
 
-import com.szmengran.chatgpt.api.UserFacade;
+import com.szmengran.authorization.api.UserFacade;
+import com.szmengran.authorization.dto.cqe.UserRegisterCmd;
 import com.szmengran.chatgpt.app.converter.Converter;
 import com.szmengran.chatgpt.dto.user.TokenCO;
 import com.szmengran.chatgpt.dto.user.TokenQueryCmd;
 import com.szmengran.chatgpt.infrastructure.oauth2.client.Oauth2Client;
 import com.szmengran.chatgpt.infrastructure.oauth2.client.dto.TokenDTO;
+import com.szmengran.cola.dto.Response;
 import jakarta.annotation.Resource;
+import org.apache.dubbo.config.annotation.DubboReference;
 
 import org.springframework.stereotype.Service;
 
@@ -16,14 +19,20 @@ import org.springframework.stereotype.Service;
  * @Date 2023/6/12 01:25
  */
 @Service
-public class UserFacadeImpl implements UserFacade {
+public class UserServiceImpl {
+
+	@DubboReference
+	private UserFacade userFacade;
 
 	@Resource
 	private Oauth2Client oauth2Client;
 
-	@Override
 	public TokenCO login(TokenQueryCmd tokenQueryCmd) {
 		TokenDTO tokenDTO = oauth2Client.getToken(tokenQueryCmd);
 		return Converter.INSTANCE.toTokenCO(tokenDTO);
+	}
+
+	public Response register(UserRegisterCmd userRegisterCmd) {
+		return userFacade.register(userRegisterCmd);
 	}
 }
