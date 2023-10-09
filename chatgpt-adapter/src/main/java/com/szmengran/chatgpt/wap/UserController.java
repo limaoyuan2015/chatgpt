2507bed9f115389e856dd7c09a2f8f59a2cddbc9
maoyuan.li@zhenai.com <maoyuan.li@zhenai.com>
2023-10-10 01:32:56 +0800

    version update

--- chatgpt-adapter/src/main/java/com/szmengran/chatgpt/wap/UserController.java
+++ chatgpt-adapter/src/main/java/com/szmengran/chatgpt/wap/UserController.java
@@ -5,13 +5,12 @@ import com.szmengran.authorization.dto.cqe.MiniProgramTokenQueryCmd;
 import com.szmengran.authorization.dto.cqe.TokenQueryCmd;
 import com.szmengran.authorization.dto.cqe.UserRegisterCmd;
 import com.szmengran.chatgpt.app.user.UserServiceImpl;
-import com.szmengran.chatgpt.dto.ChatGPTTokenCO;
-import com.szmengran.chatgpt.infrastructure.oauth2.config.ClientPrincipalProperties;
 import com.szmengran.cola.dto.Response;
 import com.szmengran.cola.dto.SingleResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,13 +28,9 @@ public class UserController {
 	@Resource
 	private UserServiceImpl userService;
 
-	@Resource
-	private ClientPrincipalProperties clientPrincipalProperties;
-	
 	@Operation(summary = "用户登录")
 	@PostMapping("/login")
 	public SingleResponse<TokenCO> passwordLogin(@RequestBody TokenQueryCmd tokenQueryCmd) {
-		tokenQueryCmd.setScope(clientPrincipalProperties.getScope());
 		TokenCO tokenCO = userService.login(tokenQueryCmd);
 		return SingleResponse.of(tokenCO);
 	}
@@ -43,7 +38,6 @@ public class UserController {
 	@Operation(summary = "小程序用户登录")
 	@PostMapping("/login/{code}")
 	public SingleResponse<TokenCO> miniProgramLogin(@PathVariable("code") String code, @RequestBody MiniProgramTokenQueryCmd miniProgramTokenQueryCmd) {
-		miniProgramTokenQueryCmd.setScope(clientPrincipalProperties.getScope());
 		miniProgramTokenQueryCmd.setCode(code);
 		TokenCO tokenCO = userService.miniProgramLogin(miniProgramTokenQueryCmd);
 		return SingleResponse.of(tokenCO);
